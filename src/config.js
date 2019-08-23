f18a97a65ee63029d47c8c7cd06a3363803cb2d4
falstack <icesilt@outlook.com>
2019-08-23 18:51:12 +0800

    init project

---
+++ src/config.js
@@ -0,0 +1,31 @@
+// 管理 .eosrc 文件 (当前用户目录下)
+
+import {
+  get, set, getAll, remove,
+} from './utils/rc';
+
+const config = async (action, key, value) => {
+  switch (action) {
+    case 'get':
+      if (key) {
+        const result = await get(key);
+        console.log(result);
+      } else {
+        const obj = await getAll();
+        Object.keys(obj).forEach((key) => {
+          console.log(`${key}=${obj[key]}`);
+        });
+      }
+      break;
+    case 'set':
+      set(key, value);
+      break;
+    case 'remove':
+      remove(key);
+      break;
+    default:
+      break;
+  }
+};
+
+module.exports = config;
